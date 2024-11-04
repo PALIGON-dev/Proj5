84e629a756ae67681bec9835dd2f2568e552c80e
PALIGON-dev <deloriansandstorm@gmail.com>
2024-11-04 17:50:17 +0300

    распаковка архива

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
-Sentece = "Something", "went", "wrong", "boring"
-for i in Sentece:
-    print(i)
+import zipfile
+
+def load_vfs(self, vfs_path):
+    # Загружаем виртуальную файловую систему из zip-архива
+    vfs = {}
+    with zipfile.ZipFile(vfs_path, 'r') as zip_ref:
+        for file_info in zip_ref.infolist():
+            if not file_info.is_dir():
+                # Сохраняем относительные пути файлов в vfs без извлечения на диск
+                vfs[file_info.filename] = zip_ref.read(file_info.filename).decode("utf-8")
+    return vfs
